52556da4b58a07b3cfbc4c3e35f9059505945895
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2025-01-16 14:19:32 +0100

    dont know whats gonig on

--- DeviceAdapters/ThorlabsMCM3001/ThorlabsMCM3001.cpp
+++ DeviceAdapters/ThorlabsMCM3001/ThorlabsMCM3001.cpp
@@ -204,6 +204,188 @@ int ThorlabsMCM3001::OnPort(MM::PropertyBase* pProp, MM::ActionType eAct)
     return DEVICE_OK;
 }
 
+int ThorlabsMCM3001::SetPositionSteps(long steps)
+{
+    MMThreadGuard guard(lock_);
+    
+    // Create goto position command
+    CmdPacket12 cmd = {
+        CMD_GOTO_POS,    // Command byte
+        0x04,            // Length
+        0x06,            // Param1 (always 0x06)
+        0x00,            // Param2
+        0x00,            // Param3
+        0x00,            // Param4
+        currentAxis_,    // Channel ID
+        steps           // Target position in encoder counts
+    };
+
+    LogMessage("Setting position to " + std::to_string(steps) + " steps");
+    
+    int ret = SendCommand(cmd);
+    if (ret != DEVICE_OK)
+    {
+        LogMessage("Failed to send goto position command");
+        return ret;
+    }
+
+    // Wait for move to complete
+    do {
+        CmdPacket6 statusCmd = {
+            CMD_REQUEST_STATUS,
+            0x04,
+            static_cast<uint8_t>(currentAxis_ & 0xFF),
+            0x00,
+            0x00,
+            0x00
+        };
+
+        ret = SendCommand(statusCmd);
+        if (ret != DEVICE_OK)
+            return ret;
+
+        StatusResponse response;
+        ret = ReadResponse(reinterpret_cast<unsigned char*>(&response), sizeof(response));
+        if (ret != DEVICE_OK)
+            return ret;
+
+        busy_ = (response.data[16] & 0x30) != 0;
+        
+        if (busy_)
+            CDeviceUtils::SleepMs(50);  // Wait before checking again
+            
+    } while (busy_);
+
+    return DEVICE_OK;
+}
+
+int ThorlabsMCM3001::GetPositionSteps(long& steps)
+{
+    MMThreadGuard guard(lock_);
+    
+    // Create query position command
+    CmdPacket6 cmd = {
+        CMD_QUERY_POS,   // Command byte
+        0x04,            // Length
+        static_cast<uint8_t>(currentAxis_ & 0xFF),  // Channel ID
+        0x00,            // Param1
+        0x00,            // Param2
+        0x00             // Param3
+    };
+
+    int ret = SendCommand(cmd);
+    if (ret != DEVICE_OK)
+    {
+        LogMessage("Failed to send position query command");
+        return ret;
+    }
+
+    // Read response
+    PosResponse response;
+    ret = ReadResponse(reinterpret_cast<unsigned char*>(&response), sizeof(response));
+    if (ret != DEVICE_OK)
+    {
+        LogMessage("Failed to read position response");
+        return ret;
+    }
+
+    // Position is in the last 4 bytes of data
+    steps = *reinterpret_cast<int32_t*>(&response.data[2]);
+    LogMessage("Current position: " + std::to_string(steps) + " steps");
+    
+    return DEVICE_OK;
+}
+
+int ThorlabsMCM3001::Home()
+{
+    MMThreadGuard guard(lock_);
+    
+    // Create home command (specific to MCM3001)
+    CmdPacket6 cmd = {
+        0x43,            // Home command
+        0x04,            // Length
+        static_cast<uint8_t>(currentAxis_ & 0xFF),  // Channel ID
+        0x00,            // Param1
+        0x00,            // Param2
+        0x00             // Param3
+    };
+
+    LogMessage("Starting homing sequence");
+    
+    int ret = SendCommand(cmd);
+    if (ret != DEVICE_OK)
+    {
+        LogMessage("Failed to send home command");
+        return ret;
+    }
+
+    // Wait for homing to complete
+    do {
+        CmdPacket6 statusCmd = {
+            CMD_REQUEST_STATUS,
+            0x04,
+            static_cast<uint8_t>(currentAxis_ & 0xFF),
+            0x00,
+            0x00,
+            0x00
+        };
+
+        ret = SendCommand(statusCmd);
+        if (ret != DEVICE_OK)
+            return ret;
+
+        StatusResponse response;
+        ret = ReadResponse(reinterpret_cast<unsigned char*>(&response), sizeof(response));
+        if (ret != DEVICE_OK)
+            return ret;
+
+        busy_ = (response.data[16] & 0x30) != 0;
+        
+        if (busy_)
+            CDeviceUtils::SleepMs(100);  // Longer wait during homing
+            
+    } while (busy_);
+
+    LogMessage("Homing completed");
+    return DEVICE_OK;
+}
+
+int ThorlabsMCM3001::SetOrigin()
+{
+    MMThreadGuard guard(lock_);
+    
+    // Create set encoder command
+    CmdPacket12 cmd = {
+        CMD_SET_ENCODER, // Command byte
+        0x04,           // Length
+        0x06,           // Param1 (always 0x06)
+        0x00,           // Param2
+        0x00,           // Param3
+        0x00,           // Param4
+        currentAxis_,   // Channel ID
+        0              // Set current position to 0
+    };
+
+    LogMessage("Setting current position as origin");
+    
+    int ret = SendCommand(cmd);
+    if (ret != DEVICE_OK)
+    {
+        LogMessage("Failed to set origin");
+        return ret;
+    }
+
+    return DEVICE_OK;
+}
+
+int ThorlabsMCM3001::GetLimits(double& lower, double& upper)
+{
+    // MCM3001 has fixed travel range of ±12.7mm for ZFM2020/ZFM2030
+    lower = -12700.0;  // micrometers
+    upper = 12700.0;   // micrometers
+    return DEVICE_OK;
+}
+
 
 
 
